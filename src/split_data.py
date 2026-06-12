cd02901c333d191764f6fb83d4f9d8d096cded5a
jpyukari <142480167+jpyukari@users.noreply.github.com>
2026-06-12 21:09:13 +0200

    Update split_data.py

--- src/split_data.py
+++ src/split_data.py
@@ -1,5 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import re
+
+def process_text(text):
+    text = text.lower()
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
 
 df = pd.read_json("./data/train.json")
 
@@ -7,6 +13,44 @@ train_df, valid_df = train_test_split(
     df,
     test_size=0.1,
     random_state=42,
+)
+
+# train側の回答語彙を作成
+train_answers = set()
+
+for answers in train_df["answers"]:
+    for a in answers:
+        train_answers.add(
+            process_text(a["answer"])
+        )
+
+# valid側で未知回答を含むサンプルをtrainへ移動
+keep_valid_idx = []
+move_to_train_idx = []
+
+for idx, row in valid_df.iterrows():
+
+    unknown = False
+
+    for a in row["answers"]:
+        ans = process_text(a["answer"])
+
+        if ans not in train_answers:
+            unknown = True
+            break
+
+    if unknown:
+        move_to_train_idx.append(idx)
+    else:
+        keep_valid_idx.append(idx)
+
+# 再構築
+valid_df = valid_df.loc[keep_valid_idx]
+extra_train = df.loc[move_to_train_idx]
+
+train_df = pd.concat(
+    [train_df, extra_train],
+    ignore_index=True
 )
 
 train_df.to_json(
